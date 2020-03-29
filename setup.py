d84f14b89925eabd61daf096ddc04bc286e2b062
Kurt Jacobson <kcjengr@gmail.com>
2020-03-29 14:23:06 -0400

    update package names

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("entry_points.ini", "r") as fh:
     entry_points = fh.read()
 
 setup(
-    name="qtpyvcp-computer-vision",
+    name="qtpyvcp.computer-vision",
     version="0.0.1",
     author="Kurt Jacobson",
-    author_email="<doe.john@example.com>",
-    description="QtPyVCP Computer Vision widgets.",
+    author_email="",
+    description="A collection of Computer Vision Widgets and Plugins for QtPyVCP ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kcjengr/qtpyvcp-computer-vision",
-    download_url="https://github.com/kcjengr/qtpyvcp-computer-vision/tarball/master",
+    url="https://github.com/kcjengr/qtpyvcp.computer-vision",
+    download_url="https://github.com/kcjengr/qtpyvcp.computer-vision/tarball/master",
     packages=find_packages(),
     include_package_data=True,
     entry_points=entry_points
